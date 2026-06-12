9cf41283cabc8a103caaa08f07c8b7d63e1adc90
SuspiciousApplePie <andreiusita6@gmail.com>
2026-06-12 21:36:48 +0800

    Add GreetingsEnd Component

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -2,4 +2,8 @@ function Greeting() {
   return <h1>"Hello, I am Shishi Hiiragi, it is not nice to meet you."</h1>;
 }
 
-export default Greeting;
+function GreetingEnd() {
+  return <h1>"Badbye."</h1>;
+}
+
+export { Greeting, GreetingEnd };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Greeting from "./Greetings.jsx";
+import { Greeting, GreetingEnd } from "./Greetings.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
+    <GreetingEnd />
   </StrictMode>,
 );
